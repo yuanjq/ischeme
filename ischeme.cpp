a1aae74da7cf628216b419797a276cfb46fc889b
yuanjq <yuanjq91@gmail.com>
2015-12-09 23:55:11 +0800

    handle alpha parse flow.

--- ischeme.cpp
+++ ischeme.cpp
@@ -1,139 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "ischeme.h"
 
-
-#define IWarning(fmt, ...)  printf("*Warning*: " fmt, ##__VA_ARGS__)
-#define IError(fmt, ...)    printf("*Error*: " fmt, ##__VA_ARGS__)
-#define IDEBUG_MORE
-
-#ifdef  IDEBUG_MORE
-#define IMessage(fmt, ...)  printf("*Message*: " fmt, ##__VA_ARGS__)
-#define ITraceEnter()     IMessage("Func %s enter.", __FUNCTION__)
-#define ITraceLeave()     IMessage("Func %s leave.", __FUNCTION__)
-#else
-#define IMessage(fmt, ...)
-#define ITraceEnter()
-#define ITraceLeave()
-#endif
-
-typedef unsigned char   UInt8;
-typedef unsigned char   Boolean;
-typedef const char*     String;
-typedef const char*     Symbol;
-typedef struct _Cell    Cell;
-typedef struct _Number  Number;
-//typedef struct _Conti   Conti;
-typedef struct _Port    Port;
-typedef struct _Conti   Conti;
-typedef struct _IScheme IScheme;
-
-#define TRUE              1
-#define FALSE             0
-#define SEGS_NUM          100
-#define SEG_CELLS_NUM     5000
-#define SEG_MEM_SIZE      (SEG_CELLS_NUM * sizeof(Cell))
-
-enum Type {SYNTAX=1, CHAR, BOOLEAN, NUMBER, STRING, SYMBOL, CONS, VECTOR, PORT, EXPR, LAMBDA, PROC, MACRO, CONTI};
-enum PortType {
-    PORT_FREE   = 0,
-    PORT_INPUT  = 1,
-    PORT_OUTPUT = 2,
-    PORT_FILE   = 4,
-    PORT_STRING = 8,
-    PORT_EOF    = 32,
-};
-
-enum Op {
-    #define _OPCODE(f, n, t, o) o,
-    #include "opcodes.h"
-    #undef _OPCODE
-    OP_MAX
-};
-
-struct _Number {
-    Boolean fixed;
-    union {
-        long l;
-        double d;
-    };
-};
-
-struct _Port {
-    int t;
-    union {
-        struct {
-            FILE *file;
-            char *name;
-        } f;
-        struct {
-          char *start;
-          char *end;
-          char *cur;
-        } s;
-    };
-};
-
-struct _Conti {
-    Op op;
-    Cell *args;
-    Cell *envir;
-    Cell *code;
-};
-
-struct _Cell {
-    Type t;
-    union {
-        Number num;
-        String str;
-        struct {
-            Cell *a;
-            Cell *d;
-        } cons;
-        Port *port;
-        Conti *conti;
-        Cell *next;
-    };
-};
-
-struct _IScheme {
-    int lastSeg;
-    int freeCellCount;
-    Cell *segs[SEGS_NUM];
-    Cell *freeCells;
-    Cell *globalEnvir;
-    Cell *symbols;
-    Cell *inPort;
-    Cell *outPort;
-
-    Op op;
-    Cell *args;
-    Cell *envir;
-    Cell *code;
-    //Cell *retValue;
-    Cell *conti;
-};
-
+#define DELIMITERS  "()[]{}\";\f\t\v\n\r "
+static IScheme g_isc = {0};
 static Cell* op_func(IScheme*, int);
-
-typedef Cell* (*OpFunc)(IScheme*, int);
-typedef struct _OpCode OpCode;
-struct _OpCode {
-    OpFunc func;
-    String name;
-    int t;
-};
-
 static OpCode g_opcodes[] = {
     #define _OPCODE(f, n, t, o) {f, n, t},
     #include "opcodes.h"
     #undef _OPCODE
     {0}
 };
-
-static IScheme g_isc = {0};
+static Reader g_readers[256];
 
 static void gc(IScheme *isc);//, Cell *args, Cell *env);
+
 
 /************** memery manager ************/
 static int seg_alloc(IScheme *isc, int num)
@@ -184,9 +66,9 @@ static void gc(IScheme *isc)//, Cell *args, Cell *env)
 static Cell *cons(Cell *a, Cell *d) {
     Cell *c = cell_alloc();
     if (c) {
-        c->t = CONS;
-        c->cons.a = a;
-        c->cons.d = d;
+        c->t = PAIR;
+        c->pair.a = a;
+        c->pair.d = d;
     }
     return c;
 }
@@ -224,7 +106,7 @@ static Cell *mkSymbol(String s)
     Cell *c = cell_alloc();
     if (c) {
         c->t = SYMBOL;
-        c->str = s;
+        c->str = strdup(s);
     }
     return c;
 }
@@ -234,8 +116,26 @@ static Cell *mkLambda(Cell *a, Cell *e)
     Cell *c = cell_alloc();
     if (c) {
         c->t = LAMBDA;
-        c->cons.a = a;
-        c->cons.d = e;
+        c->pair.a = a;
+        c->pair.d = e;
+    }
+    return c;
+}
+
+static Cell *mkPort(FILE *f, String name)
+{
+    Cell *c = cell_alloc();
+    if (c) {
+        c->t = PORT;
+        c->port = (Port*)malloc(sizeof(Port));
+        if (c->port) {
+            c->port->t = PORT_FILE;
+            c->port->f.file = f;
+            c->port->f.name = strdup(name);
+        } else {
+            IError("no memory.");
+            return NULL;
+        }
     }
     return c;
 }
@@ -260,25 +160,28 @@ static Cell *mkConti(IScheme *isc, Op op, Cell *args, Cell *code)
     return c;
 }
 
-static Boolean isCons(Cell *c)      { return c && c->t == CONS; }
+static Boolean isPair(Cell *c)      { return c && c->t == PAIR; }
 static Boolean isSymbol(Cell *c)	{ return c && c->t == SYMBOL; }
+static Boolean isPort(Cell *)       { return c && c->t == PORT; }
 
 static String symbol(Cell *c)       { return isSymbol(c) ? c->str: NULL; }
+static Port* port(Cell *c)          { return isPort(c) ? c->port : NULL; }
 
-static Cell *car(Cell *c)        { return isCons(c) ? c->cons.a : NULL; }
-static Cell *cdr(Cell *c)        { return isCons(c) ? c->cons.d : NULL; }
+static Cell *car(Cell *c)        { return isPair(c) ? c->pair.a : NULL; }
+static Cell *cdr(Cell *c)        { return isPair(c) ? c->pair.d : NULL; }
 static Cell *caar(Cell *c)       { return car(car(c)); }
 static Cell *cadr(Cell *c)       { return car(cdr(c)); }
 static Cell *cdar(Cell *c)       { return cdr(car(c)); }
 static Cell *cddr(Cell *c)       { return cdr(cdr(c)); }
 
-static Cell *rplaca(Cell *c, Cell *a)    { return isCons(c) ? c->cons.a = a : NULL; }
-static Cell *rplacd(Cell *c, Cell *d)    { return isCons(c) ? c->cons.d = d : NULL; }
+static Cell *rplaca(Cell *c, Cell *a)    { return isPair(c) ? c->pair.a = a : NULL; }
+static Cell *rplacd(Cell *c, Cell *d)    { return isPair(c) ? c->pair.d = d : NULL; }
 
 
 /***************** repl loop ******************/
-static inline int getChar(IScheme *isc) {
-    Port *p = isc->inPort->port;
+static inline int getChar(Cell *in) {
+    //if (!isPort(in)) return EOF;
+    Port *p = in->port;
     if (p->t & PORT_EOF) return EOF;
     int c = 0;
     if (p->t & PORT_FILE) {
@@ -294,10 +197,21 @@ static inline int getChar(IScheme *isc) {
     return c;
 }
 
-#define gotoOp(sc, o)      {sc->op=o; goto Begin;}
+static inline void ungetChar(Cell *out, int c) {
+    //if (!isPort(out)) return;
+    Port *p = out->port;
+    if (c == EOF) return;
+    if (p->t & PORT_FILE) {
+        ungetc(c, p->f.file);
+    } else {
+        if (p->s.cur != p->s.start) p->s.cur--;
+    }
+}
+
+#define gotoOp(sc, o)      {sc->op=o; goto Loop;}
 static Cell *op_func(IScheme *isc, int op)
 {
-Begin:
+Loop:
     switch (op) {
     case OP_LOAD:
         break;
@@ -331,7 +245,7 @@ static Cell* internal(IScheme *isc, String s)
 {
     Cell *c = NULL;
     if ((c = findSymbol(isc, s))) return c;
-    c = mkSymbol(strdup(s));
+    c = mkSymbol(s);
     isc->symbols = cons(c, isc->symbols);
     return c;
 }
@@ -361,13 +275,227 @@ static void new_envir(IScheme *isc, Cell *s, Cell *v)
     }
 }
 
-static void isc_init()
+static Cell *readIllegal(IScheme *isc, int c)
+{
+    return (Cell*)-1;
+}
+
+static Cell *readBlank(IScheme *isc, int c)
+{
+    return NULL;
+}
+
+static inline String readUpto(IScheme *isc, String s)
+{
+    char *p = isc->inBuff;
+    while ((p - isc->inBuff < sizeof(isc->inBuff)) &&
+           !strchr(s, (*p++ = getChar(isc->inPort))));
+    ungetChar(isc, *--p);
+    *p = '\0';
+    return isc->inBuff;
+}
+
+static Boolean startWith(String s, String w)
+{
+    char *p1, p2;
+    p1 = s; p2 = w;
+    while (*p1 != '\0' && *p2 != '\0' && *p1++ == *p2++);
+    if (*p2 == '\0')
+        return TRUE;
+    else
+        return FALSE;
+}
+
+static Cell *readAlpha(IScheme *isc, int c)
+{
+    ungetChar(isc->inPort, c);
+    char *str = readUpto(isc, DELIMITERS);
+    char *p = str;
+
+    Exactness exactness = NO_EXACTNESS;
+    Radix radix = NO_RADIX;
+    while (*p == '#') {
+        switch (*++p) {
+        case 'b': case 'B':
+            if (radix != NO_RADIX)
+                return RET_FAILED;
+            radix = BIN;
+            break;
+        case 'o': case 'O':
+            if (radix != NO_RADIX)
+                return RET_FAILED;
+            radix = OCT;
+            break;
+        case 'd': case 'D':
+            if (radix != NO_RADIX)
+                return RET_FAILED;
+            radix = DEC;
+            break;
+        case 'x': case 'X':
+            if (radix != NO_RADIX)
+                return RET_FAILED;
+            radix = HEX;
+            break;
+        case 'e': case 'E':
+            if (exactnes != NO_EXACTNESS)
+                return RET_FAILED;
+            exactness = EXACT;
+            break;
+        case 'i': case 'I':
+            if (exactnes != NO_EXACTNESS)
+                return RET_FAILED;
+            exactness = INEXACT;
+            break;
+        default:
+            return RET_FAILED;
+        }
+        ++p;
+    }
+
+    if (radix == NO_RADIX) {
+        radix = DEC;
+    }
+
+    //int sign =
+}
+#if 0
+static Cell *readAlpha(IScheme *isc, int c)
+{
+    char buf[1024];
+    int idx = 0;
+    buf[idx++] = c;
+    while ((c = getc(in)) > 0 && (readAlpha == _readers[c] || readNumber == _readers[c] || readSign == _readers[c]))
+        buf[idx++] = c;
+    ungetc(c, in);
+    buf[idx] = '\0';
+    return mkSymbol(buf);
+}
+#endif
+static Cell *readHash(IScheme *isc, int c)
+{
+
+}
+
+static Cell *readString(IScheme *isc, int c)
+{
+    char buf[1024];
+    int idx = 0;
+    int c;
+    while ((c = getc(in)) > 0 && c != q)
+    {
+        if ('\\' == c)
+        {
+            buf[idx++] = c;
+            buf[idx++] = getc(in);
+        }
+        else
+            buf[idx++] = c;
+    }
+    if (c != q) error("EOF in string");
+    buf[idx++] = '\0';
+    return mkString(strdup(buf));
+}
+
+static Cell *readSign(IScheme *isc, int c)
+{
+    int d = getc(in);
+    ungetc(d, in);
+    return (d > 0 && _readers[d] == readNumber) ? readNumber(c, in) : readAlpha(c, in);
+}
+
+static Cell *readQuote(IScheme *isc, int c)
+{
+    Cell *cell = readFile(in);
+    cell = cons(cell, 0);
+    cell = cons(_s_quote, cell);
+    return cell;
+}
+
+static Cell *readQuasiquote(IScheme *isc, int c)
+{
+    return 0;
+}
+
+static Cell *readUnquote(IScheme *isc, int c)
+{
+    return 0;
+}
+
+static Cell *readList(IScheme *isc, int c)
+{
+    Cell *head, *tail, *cell = 0;
+    head = tail = cons(0, 0);
+
+    switch (c) {
+    case '(': c = ')'; break;
+    case '[': c = ']'; break;
+    case '{': c = '}'; break;
+    }
+
+    int d, m = 0, n = 0;
+    for (;;) {
+        while (isspace((d = getc(in))));
+        if (c == d) break;
+        if (feof(in)) error("end of file");
+        if (d == ')' || d == ']' || d == '}' || n > 0) error("mismatched parentheses");
+        if (d == '.') {
+            if (m = 0 || n++ > 0) error("illegal use of '.'");
+            rplacd(tail, readFile(in));
+        }
+        else {
+            m++;
+            ungetc(d, in);
+            cell = readFile(in);
+            tail = rplacd(tail, cons(cell, 0));
+        }
+    }
+
+    head = cdr(head);
+    if (head && symbolP(car(head))) {
+        Cell *syntax = assq(car(head), cdr(_syntaxTable));
+        if (syntax) head = apply(cdr(syntax), cdr(head), _environment);
+    }
+
+    return head;
+}
+
+static Cell *readSemi(IScheme *isc, int c)
+{
+    while ((c = getc(in)) && c != '\n' && c != '\r');
+    return 0;
+}
+
+static void initReaders(Reader r, char *chrs)
+{
+    while (*chrs) g_readers[*chrs++] = r;
+}
+
+static void isc_init(FILE *in, String name)
 {
     g_isc.freeCellCount = 0;
     g_isc.freeCells = NULL;
     g_isc.lastSeg = -1;
     g_isc.globalEnvir = NULL;
     seg_alloc(&g_isc, 3);
+
+    g_isc.inPort = mkPort(in, name);
+    g_isc.outPort = mkPort(stdout, NULL);
+
+    for (int i = 0;  i < 256;  i++) g_readers[i]= readIllegal;
+    initReaders(readBlank,  " \t\n\v\f\r");
+    initReaders(readAlpha,  "0123456789");
+    initReaders(readAlpha,  "abcdefghijklmnopqrstuvwxyz");
+    initReaders(readAlpha,  "ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    initReaders(readAlpha,  "!#$%&*/:<=>?@\\^_|~");
+    initReaders(readAlpha,  ".");
+    initReaders(readHash,   "#");
+    initReaders(readSign,   "+-");
+    initReaders(readString, "\"");
+    initReaders(readQuote,  "'");
+    initReaders(readQuasiquote, "`");
+    initReaders(readUnquote, ",");
+    initReaders(readList,   "([{");
+    initReaders(readSemi,   ";");
 
     for (int i = 0; i < sizeof(g_opcodes)/sizeof(OpCode); i++) {
         if (g_opcodes[i].name) {
@@ -386,11 +514,29 @@ static void isc_repl()
     }
 }
 
+static void isc_finalize()
+{
+
+}
+
 int main(int argc, char *argv[])
 {
-    isc_init();
+    FILE *in = NULL;
+    String name = NULL;
+    if (argc == 1) {
+        in = stdin;
+    } else {
+        in = fopen(argv[1], "r");
+        name = argv[1];
+        if (!in) {
+            IError("cant't open file '%s'.", name);
+            return -1;
+        }
+    }
 
+    isc_init(in, name);
     isc_repl();
+    isc_finalize();
     return 0;
 }
 
--- ischeme.h
+++ ischeme.h
@@ -1,86 +1,177 @@
 #ifndef ISCHEME_H
 #define ISCHEME_H
-/*
-#t
-#f
 
+#define IWarning(fmt, ...)  printf("*Warning*: " fmt, ##__VA_ARGS__)
+#define IError(fmt, ...)    printf("*Error*: " fmt, ##__VA_ARGS__)
+#define IDEBUG_MORE
 
-"lambda"
-"define"
-"if"
-"case"
-"set!"
-"let-syntax"
-"letrec-syntax"
-"syntax-rules"
-"when"
-"begin"
-"and"
-"or"
-"let"
-"cond" ["else"]
-"let*"
-"letrec"
-"do"
-"delay"
-"quote" // '
-"quasiquote" // `
-"unquote" // ,
-"unquote-splicing" // @
+#ifdef  IDEBUG_MORE
+#define IMessage(fmt, ...)  printf("*Message*: " fmt, ##__VA_ARGS__)
+#define ITraceEnter()     IMessage("Func %s enter.", __FUNCTION__)
+#define ITraceLeave()     IMessage("Func %s leave.", __FUNCTION__)
+#else
+#define IMessage(fmt, ...)
+#define ITraceEnter()
+#define ITraceLeave()
+#endif
 
-"eqv?"
-"eq?"
-"equal?"
-"string=?"
-"list"
-"vector-set!"
-"force"
-"eval"
-"apply"
-"car"
-"cdr"
-"caar"
-"cadr"
-"cdar"
-"cddr"
-"caaar"
-"caadr"
-...
-"not"
-"call-with-current-continuation"
-"call/cc"
-"string-set!"
-"symbol->string"
-"list->vector"
-"list?"
-"boolean?"
-"symbol?"
-"char?"
-"vector?"
-"procedure?"
-"pair?"
-"number?"
-"string?"
-"port?"
-"complex?"
-"real?"
-"rational?"
-"integer?"
-"length"
-"vector-length"
-"string-length"
-"for-each"
-"append"
-"set-car!"
-"set-cdr!"
-"char->integer"
-"integer->char"
-"char-upcase"
-"char-downcase"
+typedef unsigned char   uint8;
+typedef unsigned int    Char;
+typedef unsigned char   Boolean;
+typedef const char*     String;
+typedef const char*     Symbol;
 
-"cons"
-"assq"
-*/
+typedef struct _Cell    Cell;
+typedef struct _Char    Char;
+typedef struct _Number  Number;
+typedef struct _Port    Port;
+typedef struct _Conti   Conti;
+typedef struct _OpCode OpCode;
+typedef struct _IScheme IScheme;
 
+typedef Cell* (*OpFunc)(IScheme*, int);
+typedef Cell*(*Reader)(IScheme*, int);
+
+#define TRUE              1
+#define FALSE             0
+#define SEGS_NUM          100
+#define SEG_CELLS_NUM     5000
+#define SEG_MEM_SIZE      (SEG_CELLS_NUM * sizeof(Cell))
+
+enum Ret {
+    RET_FAILED = -1,
+    RET_SUCCESSED = 0
+};
+
+enum Type {
+    SYNTAX=1,
+    CHAR,
+    BOOLEAN,
+    NUMBER,
+    STRING,
+    SYMBOL,
+    LIST,
+    PAIR,
+    VECTOR,
+    PORT,
+    EXPR,
+    LAMBDA,
+    PROC,
+    MACRO,
+    CONTI
+};
+
+enum PortType {
+    PORT_FREE   = 0,
+    PORT_INPUT  = 1,
+    PORT_OUTPUT = 2,
+    PORT_FILE   = 4,
+    PORT_STRING = 8,
+    PORT_EOF    = 32,
+};
+
+enum NumberType {
+    NUMBER_INTEGER,
+    NUMBER_FRACTION,
+    NUMBER_RATIONAL,
+    NUMBER_REAL,
+    NUMBER_COMPLEX,
+};
+
+enum Exactness {
+    NO_EXACTNESS,
+    INEXACT,
+    EXACT
+};
+
+enum Radix {
+    NO_RADIX = 0,
+    BIN = 2,
+    OCT = 8,
+    DEC = 10,
+    HEX
+};
+
+enum Op {
+    #define _OPCODE(f, n, t, o) o,
+    #include "opcodes.h"
+    #undef _OPCODE
+    OP_MAX
+};
+
+struct _Char {
+
+};
+
+struct _Number {
+    uint8 t;
+    union {
+        long i;
+        double r;
+
+    };
+};
+
+struct _Port {
+    int t;
+    union {
+        struct {
+            FILE *file;
+            char *name;
+        } f;
+        struct {
+          char *start;
+          char *end;
+          char *cur;
+        } s;
+    };
+};
+
+struct _Conti {
+    Op op;
+    Cell *args;
+    Cell *envir;
+    Cell *code;
+};
+
+struct _Cell {
+    Type t;
+    union {
+        Char chr;
+        Number num;
+        String str;
+        struct {
+            Cell *a;
+            Cell *d;
+        } pair;
+        Port *port;
+        Conti *conti;
+        Cell *next;
+    };
+};
+
+struct _IScheme {
+    int lastSeg;
+    int freeCellCount;
+    Cell *segs[SEGS_NUM];
+    Cell *freeCells;
+    Cell *globalEnvir;
+    Cell *symbols;
+    Cell *inPort;
+    Cell *outPort;
+
+    Op op;
+    Cell *args;
+    Cell *envir;
+    Cell *code;
+    Cell *conti;
+    char inBuff[1024];
+};
+
+struct _OpCode {
+    OpFunc func;
+    String name;
+    int t;
+};
 
 #endif
